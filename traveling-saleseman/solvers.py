4b21065062543b57093a98a4c216b5fb6a595a3f
schiebermc <matthewschieber@gmail.com>
2021-02-03 23:07:38 -0500

    adds branch and bounds, random sampling, and nearest neighbor search

--- traveling-saleseman/solvers.py
+++ traveling-saleseman/solvers.py
@@ -1,11 +1,18 @@
 """
-This module implements the solver classes
+    Solvers for TSP by Matthew Schieber
+    All implementations were concieved and iterated by me.
+    Please make any suggestions at https://github.com/schiebermc/Game_AI
+
+    Current Status:
+        - very basic solvers
 
 """
 import abc
-from utils import totalDistance
-from itertools import permutations
-
+from copy import deepcopy
+from random import shuffle, seed, randint
+from collections import defaultdict
+from utils import distance, totalDistance
+from itertools import permutations, combinations
 
 class BaseSolver(abc.ABC):
 
@@ -22,6 +29,9 @@ class BaseSolver(abc.ABC):
 class HorizontalSortSolver(BaseSolver):
 
     name = "HorizontalSort"
+    
+    # *very* approximate
+    # TC: O(nlogn)
 
     def __init__(self, n, m, points):
         BaseSolver.__init__(self, n, m, points)
@@ -35,6 +45,9 @@ class VerticalSortSolver(BaseSolver):
 
     name = "VerticalSort"
 
+    # *very* approximate
+    # TC: O(nlogn)
+
     def __init__(self, n, m, points):
         BaseSolver.__init__(self, n, m, points)
 
@@ -43,9 +56,27 @@ class VerticalSortSolver(BaseSolver):
         return self.points
 
 
+class OriginSortSolver(BaseSolver):
+
+    name = "OriginSort"
+
+    # *very* approximate (last one i promise xD)
+    # TC: O(nlogn)
+
+    def __init__(self, n, m, points):
+        BaseSolver.__init__(self, n, m, points)
+
+    def computePath(self):
+        self.points.sort(key=lambda x:(x[0]**2 + x[1]**2))
+        return self.points
+
+
 class BruteForceSolver(BaseSolver):
 
     name = "BruteForce"
+    
+    # exact 
+    # TC: O(n!)
 
     def __init__(self, n, m, points):
         BaseSolver.__init__(self, n, m, points)
@@ -60,6 +91,186 @@ class BruteForceSolver(BaseSolver):
                 best = [dist, perm]
         return best[1]
 
+
+class HeldKarpSolver(BaseSolver):
+
+    name = "Held-Karp"
     
+    # exact 
+    # TC: O(2^nsqrt(n))
+
+    def __init__(self, n, m, points):
+        BaseSolver.__init__(self, n, m, points)
+
+    def computePath(self):
+
+        # NOTE: this algorithm is assuming first postion is starting point
+        # A workaround is required: 
+        # 1) iterate over all points or
+        # 2) add this assumption to other algorithms.
+        for i in range(len(self.points)):
+            points = self.points[i:] + self.points[:i]
+            self.hk(points)
+
+
+    def hk(self, points):
+
+        # .. it's not quite right, and currently does not return path. 
+        # so needs worked up.
+
+        # init the distance matrix
+        k = len(points)
+        c = [[0] * k for i in range(k)]
+        for i in range(k):
+            for j in range(k):
+                c[i][j] = distance(points[i], points[j])
+
+        g = defaultdict(dict)
+        for i in range(1, k):
+            g[tuple([i])][i] = c[i][0]
+
+        inds = [i for i in range(1, k)]
+        for s in range(2, k):
+            for combo in combinations(inds, s):
+                for k in combo:
+                    best = float('inf')
+                    for m in combo:
+                        if m == k:
+                            continue
+                        
+                        # here I take the set difference, keeping
+                        # in mind that the tuple must be lexographically
+                        # sorted in order for the DP to work correctly
+                        diff = list(set(combo) - set([k]))
+                        diff.sort()
+                        diff = tuple(diff)
+                        
+                        best = min(best, g[diff][m] + c[m][k])
+                    
+                    g[combo][k] = best
+        
+        best = float('inf')
+        for i in range(1, k):
+            best = min(best, g[tuple(inds)][i] + c[i][0])
+
+        # whoops, need to return the path associated with best, 
+        # but that info was lost along the way. need to circle back to this
+        return points
+
+
+class RandomSampleSolver(BaseSolver):
+
+    name = "RandomSampler"
+    
+    # approximate! 
+    # TC: O(N) 
+
+    def __init__(self, n, m, points, samples=1000):
+        BaseSolver.__init__(self, n, m, points)
+        self.samples = samples
+
+    def computePath(self):
+        best = [float('inf'), None]
+        for i in range(self.samples):
+            shuffle(self.points)            
+            dist = totalDistance(self.points)
+            if dist < best[0]:
+                best = [dist, deepcopy(self.points)]
+        return best[1]
+
+
+class BBSolver(BaseSolver):
+
+    name = "BranchAndBound"
+    
+    # exact 
+    # TC: O(N!) (but prunes a lot. do not use past ~30 points)
+
+    def __init__(self, n, m, points):
+        BaseSolver.__init__(self, n, m, points)
+
+    def computePath(self):
+        
+        self.best = [float('inf'), None]
+        self.curr = []
+
+        def dfs(last, todo, dist=0):
+
+            # prune if this branch cannot beat an already logged optima            
+            if dist >= self.best[0]:
+                return
+
+            if len(todo) == 0:
+                self.best = [dist, deepcopy(self.curr)]
+                # every time we get here, we have something better :)
+                print(self.best)
+
+            # try all remaining points
+            vals = list(todo)
+            for i in vals:
+                todo.remove(i)
+                self.curr.append(i)
+                dfs(i, todo, dist + distance(self.points[last], self. points[i]))
+                todo.add(i)
+                self.curr.pop()
+
+        # iterate over all starting points
+        for i in range(len(self.points)):
+            todo = set([j for j in range(len(self.points))])
+            todo.remove(i)
+            self.curr = [i]
+            dfs(i, todo)            
+
+        return [self.points[i] for i in self.best[1]]
+
+
+class NearestNeighborSolver(BaseSolver):
+
+    name = "NearestNeighbor"
+    
+    # approximate
+    # TC: O(N^3) 
+
+    def __init__(self, n, m, points):
+        BaseSolver.__init__(self, n, m, points)
+
+    def computePath(self):
+        
+        c = []
+        k = len(self.points)
+        for i in range(k):
+            c.append([])
+            for j in range(k):
+                # note: randint here is just used as a tiebreaker in the event that two cities
+                # are equidistant from curr. Without seeding, this would be nondeterministic
+                c[i].append((distance(self.points[i], self.points[j]), randint(1, 100), j)) 
+
+        for i in range(k):
+            c[i].sort()
+
+        seed(0)
+        best = [float('inf'), None]
+        for start in range(k):
+
+            dist = 0      
+            used = set([start])
+            curr = start
+            res = [start]
+
+            while(len(used) != k):
+
+                for this_dist, tiebreaker, j in c[curr]:
+                    if not j in used:
+                        dist += this_dist
+                        curr = j
+                        break
+
+                used.add(curr)
+                res.append(curr)
+                
+            if dist < best[0]:
+                best = [dist, res]
+
+        return [self.points[i] for i in best[1]]
 
 
--- traveling-saleseman/tester.py
+++ traveling-saleseman/tester.py
@@ -2,6 +2,7 @@
 This module is currently a sandbox for trying out different solvers
 
 """
+import time
 import numpy as np
 from solvers import *
 from copy import deepcopy
@@ -11,9 +12,10 @@ from random import randint, seed
 from utils import totalDistance
 
 TestSet = namedtuple("TestSet", ['points', 'n', 'm'])
+TestPackage = namedtuple("TestPackage", ['test_set_funs', 'algos'])
 
 def generateTestSet1():
-    # static test set for testing
+    # small enough for BruteForce to work (30s)
     n = m = 20
     n_points = 10
     seed(0)
@@ -21,31 +23,72 @@ def generateTestSet1():
     return TestSet(points, n, m) 
 
 
-def printDistanceAndPlot(points, name):
+def generateTestSet2():
+    # larger, last one a python Branch and Bound solver can do (120s)
+    n = m = 50
+    n_points = 12
+    seed(0)
+    points = list(set([(randint(0, m-1), randint(0, n-1)) for ex in range(n_points)])) 
+    return TestSet(points, n, m) 
 
-    print("Path: {}".format(points))
+
+def generateTestSet3():
+    # larger, last one a python Branch and Bound solver can do (120s)
+    n = m = 200
+    n_points = 500
+    seed(0)
+    points = list(set([(randint(0, m-1), randint(0, n-1)) for ex in range(n_points)])) 
+    return TestSet(points, n, m) 
+
+
+def generateTestSet4():
+    # larger, last one a python Branch and Bound solver can do (120s)
+    n = 50
+    m = 1000
+    n_points = 500
+    seed(0)
+    points = list(set([(randint(0, m-1), randint(0, n-1)) for ex in range(n_points)])) 
+    return TestSet(points, n, m) 
+
+
+def printDistanceAndPlot(points, name, total_time):
+    # plot the path, total distance, and time
     total_distance = totalDistance(points)
-    
     plt.plot(*np.asarray(points).T, marker='s')
-    plt.title("Path Traveled Using Algorithm: {}\nTotal Distance: {}".\
-        format(name, round(totalDistance(points), 2)))
+    plt.title("Path Traveled Using Algorithm: {}\nTotal Distance: {} - Time: {:0.3e}".\
+        format(name, round(totalDistance(points), 2), total_time))
     plt.show()
 
 
-test_these = [HorizontalSortSolver, VerticalSortSolver, BruteForceSolver]
+if __name__ == "__main__":
 
-
-for class_name in test_these: 
+    # define the tests you want to try
+    test1 = TestPackage(
+                        [generateTestSet3, generateTestSet4],
+                        [HorizontalSortSolver, VerticalSortSolver, OriginSortSolver, NearestNeighborSolver]
+                        )
     
-    # use test set of choice    
-    test_set = generateTestSet1()
-    points, n, m = test_set.points, test_set.n, test_set.m    
+    for test_set_fun in test1.test_set_funs:
+        
+        for class_name in test1.algos: 
+            
+            # retrieve test set
+            test_set = test_set_fun()
+            points, n, m = test_set.points, test_set.n, test_set.m    
+        
+            # create solver
+            solver = class_name(n, m, points)
+           
+            # solve 
+            t0 = time.time()
+            points = solver.computePath()
+            total_time = time.time() - t0        
 
-    # create solver
-    solver = class_name(n, m, points)
-   
-    # solve and evaluate 
-    points = solver.computePath()
-    printDistanceAndPlot(points, solver.name)
+            # evaluate
+            printDistanceAndPlot(points, solver.name, total_time)
+
+
+
+
 
 
